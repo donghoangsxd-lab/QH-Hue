c4d245860fab6f2799ceae94590858fde5a4c238
donghoangsxd-lab <donghoangsxd@gmail.com>
2026-01-21 20:39:19 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,4 @@
+const qhData = [
+  ["QH001", "Quy hoạch Phú Xuân", "123/QĐ-UBND", "01/01/2024", "UBND tỉnh", 4300, 310000],
+  ["QH002", "Quy hoạch Thuận Hóa", "456/QĐ-UBND", "15/03/2024", "UBND tỉnh", 6600, 410000]
+];
