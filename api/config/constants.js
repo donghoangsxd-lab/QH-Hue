347454be3bb28562a55d46dcf9b91c20eaf88930
donghoangsxd-lab <donghoangsxd@gmail.com>
2026-09-12 18:33:37 +0700

    Update constants.js

--- api/config/constants.js
+++ api/config/constants.js
@@ -1,8 +1,18 @@
 module.exports = {
-  GEOJSON_CACHE_TTL: 10 * 60 * 1000,   // Cache GCS 10 phút
+  GEOJSON_CACHE_TTL: 10 * 60 * 1000,    // Cache GCS 10 phút
   WARD_STATS_CACHE_TTL: 15 * 60 * 1000, // Cache Wards 15 phút
   GCS_URL: "https://storage.googleapis.com/hue-infra-data-us/infrastructure_hue.json",
   GAS_BASE_URL: "https://script.google.com/macros/s/AKfycbzyvYP9WoDizfwb-ZMT374jHbLY02X3HlhxKnmZEYl8UrYrO6SSzSB7eQRH0kaXWguU/exec",
+
+  // Cấu hình thuật toán Isochrone Giao thông (90% Di chuyển + 10% Offset làm mịn)
+  ISOCHRONE_CONFIG: {
+    REACH_RATIO: 0.9,  // 90% bán kính di chuyển thực tế theo đường giao thông
+    OFFSET_RATIO: 0.1, // 10% bán kính đệm làm mịn polygon
+    SAMPLE_ANGLES: 12  // 12 hướng bức xạ tính toán polygon
+  },
+
+  // Danh sách mã nhóm hạ tầng tiêu chuẩn phục vụ đánh giá quy chuẩn
+  CODES_TO_CHECK: ["1-CV", "2-BDX", "3-MN", "4-TH", "5-THCS", "6-YT", "7-VH", "8-TM"],
   
   quotaConfig: {
     "1-CV": 7.00, "2-BDX": 2.50, "3-MN": 0.60, "4-TH": 0.65,
